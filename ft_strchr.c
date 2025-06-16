352d5b3043675ed5fbe142cb9a98b875ec65e1c3
Ardovski <uardaozdes@gmail.com>
2025-06-16 16:01:10 +0300

    sa

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: uardaozdes <uardaozdes@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 09:54:45 by uardaozdes        #+#    #+#             */
-/*   Updated: 2025/06/03 13:32:30 by uardaozdes       ###   ########.fr       */
+/*   Updated: 2025/06/16 10:33:21 by uardaozdes       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strchr(const char *s, int c)
 	while (*s != '\0')
 	{
 		if (*s == (char)c)
-			return ((char *)s);
+			return ((unsigned char *)s);
 		s++;
 	}
 	if (c == '\0')
--- main.c
+++ main.c
@@ -6,76 +6,42 @@
 /*   By: uardaozdes <uardaozdes@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 11:25:19 by uardaozdes        #+#    #+#             */
-/*   Updated: 2025/06/13 18:08:09 by uardaozdes       ###   ########.fr       */
+/*   Updated: 2025/06/16 15:00:29 by uardaozdes       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
 #include "libft.h"
-#include <stdio.h>
-#include "libft.h"
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-static void d(void *content)
-{
-	if(content)
-	{
-		free(content);
-		printf("FREELENDI");
-	}
-}
 
-#include "libft.h"
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-
-// del_string burada yukarıda tanımlandı
-void	listeyi_yazdir(t_list *lst)
+void dummy_free(void *content)
 {
-	printf("Liste içeriği:\n");
-	while (lst)
-	{
-		printf("- %s\n", (char *)lst->content);
-		lst = lst->next;
-	}
+    (void)content;
 }
 
 int	main(void)
 {
-	// Veriler
-	char *veri1 = strdup("Arda");
-	char *veri2 = strdup("PLC");
-	char *veri3 = strdup("Robotik");
+    t_list	*old = NULL;
+    t_list	*new;
+    t_list	*new2;
+	t_list	*new3;
+    char	*a = "Ardovski";
+    char	*b = "Umut";
+	char	*c = "Naber";
+	
+    new = ft_lstnew(a);
+    printf("%s\n", (char *)new->content);
+    new2 = ft_lstnew(b);
+    ft_lstadd_front(&old,new);
+    printf("%s\n", (char *)old->content);
+    ft_lstadd_front(&old,new2);
+    printf("%s\n", (char *)old->content);
+    printf("%s\n", (char *)old->next->content);
+    printf("%d\n", ft_lstsize(old));
+    printf("%s\n", (char *)ft_lstlast(old)->content);
+	new3 = ft_lstnew(c);
+    ft_lstadd_back(&old,new3);
+    printf("%s\n",(char *)ft_lstlast(old)->content);
 
-	// Düğümler
-	t_list *n1 = ft_lstnew(veri1);
-	t_list *n2 = ft_lstnew(veri2);
-	t_list *n3 = ft_lstnew(veri3);
-
-	// Listeyi oluştur: Arda -> PLC -> Robotik
-	n1->next = n2;
-	n2->next = n3;
-	t_list *liste = n1;
-
-	// Önceki hâlini yazdır
-	printf("Silmeden önce:\n");
-	listeyi_yazdir(liste);
-
-	// PLC düğümünü (n2) silelim
-	n1->next = n3; // Aradaki bağlantıyı atla
-	ft_lstdelone(n2, d);
-
-	// Sonraki hâlini yazdır
-	printf("\nSildikten sonra:\n");
-	listeyi_yazdir(liste);
-
-	// Temizlik
-	ft_lstdelone(n1, d);
-	ft_lstdelone(n3, d);
-
-	return 0;
+	ft_lstclear(&old,dummy_free);
+	// ft_lstdelone(ft_lstlast(old), dummy_free);
+    // printf("%s\n",(char *)ft_lstlast(old)->content);
 }
-
-
